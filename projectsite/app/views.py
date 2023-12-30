10fce9035635b14e152fc5eb8caeedaa5840ef66
VinceBorgyEspina <202180167@psu.palawan.edu.ph>
2023-12-31 01:17:39 +0800

    DONEEEEE butttt di ma connect ang css buti have bootstrap HappyNewYear

--- projectsite/app/views.py
+++ projectsite/app/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Category, Product, Customer, Order, OrderItem
@@ -6,6 +7,7 @@ class HomePageView(ListView):
     model = Category
     context_object_name = 'home'
     template_name = "base.html"
+    paginate_by = 3  # Set the number of items per page
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -15,6 +17,7 @@ class CategoryListView(ListView):
     model = Category
     template_name = 'category_list.html'
     context_object_name = 'categories'
+    paginate_by = 3  # Set the number of items per page
 
 class CategoryDetailView(DetailView):
     model = Category
@@ -25,6 +28,7 @@ class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
     context_object_name = 'products'
+    paginate_by = 3  # Set the number of items per page
 
 class ProductDetailView(DetailView):
     model = Product
@@ -35,6 +39,7 @@ class CustomerListView(ListView):
     model = Customer
     template_name = 'customer_list.html'
     context_object_name = 'customers'
+    paginate_by = 3  # Set the number of items per page
 
 class CustomerDetailView(DetailView):
     model = Customer
@@ -45,6 +50,7 @@ class OrderListView(ListView):
     model = Order
     template_name = 'order_list.html'
     context_object_name = 'orders'
+    paginate_by = 3  # Set the number of items per page
 
 class OrderDetailView(DetailView):
     model = Order
@@ -55,6 +61,7 @@ class OrderItemListView(ListView):
     model = OrderItem
     template_name = 'order_item_list.html'
     context_object_name = 'order_items'
+    paginate_by = 3  # Set the number of items per page
 
 class OrderItemDetailView(DetailView):
     model = OrderItem
--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app', 
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
